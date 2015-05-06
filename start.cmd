8c477908dbcbfae70937539177c6405250d90d07
konenkov <konenkov@iac.spb.ru>
2015-05-06 12:34:31 +0300

    added parametr to start.cmd

--- start.cmd
+++ start.cmd
@@ -1,4 +1,4 @@
 SET APP_PATH=%~dp0
 SET LIBRARY_PATH=%APP_PATH%lib\
 
-%JAVA_HOME%\jre\bin\java.exe -cp .;%LIBRARY_PATH%* ru.iac.MainParser
+%JAVA_HOME%\jre\bin\java.exe -DXms2000m -DXmx8000m -cp .;%LIBRARY_PATH%* ru.iac.MainParser
